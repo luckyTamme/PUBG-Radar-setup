835733d7f1d5ebe4c0300c7ca54990e0e2a97712
zecjy <zecjy@outlook.de>
2018-04-08 15:58:48 +0200

    Update pubg-radar.sh

--- pubg-radar.sh
+++ pubg-radar.sh
@@ -3,64 +3,41 @@ apt-get update
 
 sysctl -w net.ipv4.ip_forward=1
 
-mkdir PUBG-Radar
-
-cd PUBG-Radar
+mkdir Radar
+cd Radar
 
 mkdir /opt/Oracle_Java
-
 wget "ftp.osuosl.org/pub/funtoo/distfiles/oracle-java/jdk-8u152-linux-x64.tar.gz" -p "/opt/Oracle_Java"
-
+rm "ftp.osuosl.org/pub/funtoo/distfiles/oracle-java/jdk-8u152-linux-x64.tar.gz"
 tar -xzf "/opt/Oracle_Java/jdk-8u152-linux-x64.tar.gz"
 
 update-alternatives --install "/usr/bin/java" "java" "/opt/Oracle_Java/jdk1.8.0_152/bin/java" 1
-
 update-alternatives --install "/usr/bin/javac" "javac" "/opt/Oracle_Java/jdk1.8.0_152/bin/javac" 1
-
 update-alternatives --install "/usr/bin/javaws" "javaws" "/opt/Oracle_Java/jdk1.8.0_152/bin/javaws" 1
-
 update-alternatives --install "/usr/bin/jar" "jar" "/opt/Oracle_Java/jdk1.8.0_152/bin/jar" 1
 
-
 update-alternatives --set "java" "/opt/Oracle_Java/jdk1.8.0_152/bin/java"
-
 update-alternatives --set "javac" "/opt/Oracle_Java/jdk1.8.0_152/bin/javac"
-
 update-alternatives --set "javaws" "/opt/Oracle_Java/jdk1.8.0_152/bin/javaws"
-
 update-alternatives --set "jar" "/opt/Oracle_Java/jdk1.8.0_152/bin/jar"
-
-apt-get -y install maven
 
 apt-get -y install dsniff
 
-apt-get -y install git
-
-git clone "https://github.com/Jerry1211/PUBG-Radar"
-
-cd PUBG-Radar
-
-mvn install
-
-cd target/
+wget -O "Radar-4k.jar" "https://www.xup.in/dl,58876555/Albedo-0.1-jar-with-dependencies.jar/"
 
 clear
-
 echo "Ip Adresse des Game PC eingeben (192.168.??.??) und Enter druecken"
 read game_ip
 
 clear
-
 echo "Ip Adresse des Router eingeben (192.168.??.??) und Enter druecken"
 read router_ip
 
 clear
-
 echo "Ip Adresse des Radar PC eingeben (192.168.??.??) und Enter druecken"
 read radar_ip
 
 clear
-
 echo "Gebe den name des zu nutzenden Interface ein"
 ls /sys/class/net
 
