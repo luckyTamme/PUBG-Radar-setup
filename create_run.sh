0bfca8d8895b8c9da594a34996d4b0835f5b6e94
ZeCjy <zecjy@outlook.de>
2018-04-19 19:52:08 +0200

    arpspoof output
    arpspoof output is now hidden by default and can be shown by "run.sh --show_arpspoof"

--- create_run.sh
+++ create_run.sh
@@ -21,8 +21,14 @@ read interface
 cat >run.sh <<EOF
 #!/bin/bash
 sysctl -w net.ipv4.ip_forward=1
-arpspoof -i $interface -t $game_ip $router_ip & >/dev/null
-arpspoof -i $interface -t $router_ip $game_ip & >/dev/null
+if [ "$1" == '--show_arpspoof' ]
+then
+  arpspoof -i $interface -t $game_ip $router_ip
+  arpspoof -i $interface -t $router_ip $game_ip
+else
+  arpspoof -i $interface -t $game_ip $router_ip &> /dev/null
+  arpspoof -i $interface -t $router_ip $game_ip &> /dev/null
+fi
 java -jar PUBG-Radar/target/Gaydar-6.9-jar-with-dependencies.jar $radar_ip PortFilter $game_ip
 
 EOF
